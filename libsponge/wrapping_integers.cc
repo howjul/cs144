a7d87db73aee8eff1a18e446007a3a368b2c902a
Haozhe Zhu <dyzhz123@163.com>
2023-11-10 15:31:05 +0800

    lab4 任务二完成

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,8 +14,9 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint64_t x = 1ll << 32;
+    uint32_t res = (isn.raw_value() + n) % x;
+    return WrappingInt32{res};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +30,13 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint64_t x = 1ll << 32;
+    uint64_t res = (uint64_t)(n.raw_value() - isn.raw_value());
+    if(res >= checkpoint) return res;
+    else{
+        uint64_t differ = checkpoint - res + (x >> 1);
+        uint64_t differ_multi = differ / x;
+        res += differ_multi * x;
+        return res;
+    }
 }
